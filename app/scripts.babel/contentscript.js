075ab393bb60ab1e920c8fc133af409eafea79c7
Lukáš Marek <lukas.marek@gmail.com>
2016-09-28 17:11:55 +0200

    Bezrealitky PoC

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+window.addEventListener('load', function() {
+
+  var address = $('h2').first().text();
+
+  $.get(chrome.extension.getURL('/panel.html'), function(data) {
+    $('.box-owner.col-1').before(data);
+    $('h3.address').html(address);
+    // Or if you're using jQuery 1.8+:
+    // $($.parseHTML(data)).appendTo('body');
+  });
+});
