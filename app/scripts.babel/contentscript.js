f79f2288c004377c619654406d716f9658cc1557
Lukáš Marek <lukas.marek@gmail.com>
2016-10-01 12:01:29 +0200

    Add data from liftago

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var KEY = 'AIzaSyDP6X1_N95A5pEKOyNgzWNtRK04sL12oek';
+var NODE5 = {
+  lat: 50.0663614,
+  lng: 14.4005557
+};
 
 var _init = function() {
   var cssPath = chrome.extension.getURL('/styles/panel.css');
@@ -12,15 +16,23 @@ var _loadPubs = function(location) {
     '&radius=500&types=restaurant&key=' + KEY);
 };
 
+var _loadLiftago = function(location) {
+  return $.get('http://54.93.66.14:8000/api?t=1468582200&pickup='+ location.lat + ',' + location.lng +
+  '&dest=' + NODE5.lat + ',' + NODE5.lng);
+};
+
 var _loadPanel = function(address) {
   $.get(chrome.extension.getURL('/panel.html'), function(html) {
 
     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + KEY, function(response) {
-      console.log(response.results[0].geometry.location);
-      var pubs = _loadPubs(response.results[0].geometry.location);
-  
-      $.when(pubs).done(function(pubsData) {
-        html = html.replace('@@HOSPOD@@', pubsData.results.length);
+      var location = response.results[0].geometry.location;
+      var pubsP = _loadPubs(location);
+      var liftagoP = _loadLiftago(location);
+
+      $.when(pubsP, liftagoP).done(function(pubs, liftago) {
+        html = html.replace('@@HOSPOD@@', pubs[0].results.length);
+        html = html.replace('@@LIFTAGO_NODE5@@', liftago[0][0].price + 'Kč');
+        console.log(liftago);
         $('body').append(html);
       });
     });
