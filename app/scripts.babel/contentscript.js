0e5494766ea888c37accdcb3096ddfab8306e2e0
Lukáš Marek <lukas.marek@gmail.com>
2016-10-01 11:38:49 +0200

    Show number of restaurants around

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,17 +4,26 @@ var KEY = 'AIzaSyDP6X1_N95A5pEKOyNgzWNtRK04sL12oek';
 
 var _init = function() {
   var cssPath = chrome.extension.getURL('/styles/panel.css');
-  $('head').append('<link rel="stylesheet" href="'+ cssPath + '" type="text/css" />');
+  $('head').append('<link rel="stylesheet" href="' + cssPath + '" type="text/css" />');
+};
+
+var _loadPubs = function(location) {
+  return $.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + location.lat + ',' + location.lng +
+    '&radius=500&types=restaurant&key=' + KEY);
 };
 
 var _loadPanel = function(address) {
-  $.get(chrome.extension.getURL('/panel.html'), function(data) {
+  $.get(chrome.extension.getURL('/panel.html'), function(html) {
+
     $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + KEY, function(response) {
       console.log(response.results[0].geometry.location);
+      var pubs = _loadPubs(response.results[0].geometry.location);
+  
+      $.when(pubs).done(function(pubsData) {
+        html = html.replace('@@HOSPOD@@', pubsData.results.length);
+        $('body').append(html);
+      });
     });
-
-
-    $('body').append(data);
     //
   });
 };
