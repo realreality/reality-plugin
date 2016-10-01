e238ff939b6052be3c210501e030a52099addb0a
michalbcz <michal@bernhard.cz>
2016-10-01 12:28:32 +0200

    add transit times to some places

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var KEY = 'AIzaSyDP6X1_N95A5pEKOyNgzWNtRK04sL12oek';
+var API_KEY = 'AIzaSyDP6X1_N95A5pEKOyNgzWNtRK04sL12oek';
 var NODE5 = {
   lat: 50.0663614,
   lng: 14.4005557
@@ -13,7 +13,7 @@ var _init = function() {
 
 var _loadPubs = function(location) {
   return $.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + location.lat + ',' + location.lng +
-    '&radius=500&types=restaurant&key=' + KEY);
+    '&radius=500&types=restaurant&key=' + API_KEY);
 };
 
 var _loadLiftago = function(location) {
@@ -21,22 +21,35 @@ var _loadLiftago = function(location) {
   '&dest=' + NODE5.lat + ',' + NODE5.lng);
 };
 
+var _loadTransitAvailibility = function(address) {
+  const DESTINATIONS = 'Muzeum,Praha|Radlická 180/50, Praha'; // Node5 = Radlická 180/50, Praha
+  const MAPS_API_BASE_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+
+  var distanceMatrixApiUrl = MAPS_API_BASE_URL + '?origins=' + encodeURI(address) + '&destinations=' + encodeURI(DESTINATIONS) + '&mode=transit&language=cs&key=' + API_KEY;
+  return $.get(distanceMatrixApiUrl);
+};
+
 var _loadPanel = function(address) {
   $.get(chrome.extension.getURL('/panel.html'), function(html) {
 
-    $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + KEY, function(response) {
-      var location = response.results[0].geometry.location;
-      var pubsP = _loadPubs(location);
-      var liftagoP = _loadLiftago(location);
+      $.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + encodeURI(address) + '&key=' + API_KEY, function(response) {
+          var location = response.results[0].geometry.location;
+          var pubsP = _loadPubs(location);
+          var liftagoP = _loadLiftago(location);
+          var transitP = _loadTransitAvailibility(address);
 
-      $.when(pubsP, liftagoP).done(function(pubs, liftago) {
-        html = html.replace('@@HOSPOD@@', pubs[0].results.length);
-        html = html.replace('@@LIFTAGO_NODE5@@', liftago[0][0].price + 'Kč');
-        console.log(liftago);
-        $('body').append(html);
-      });
-    });
-    //
+          $.when(pubsP, liftagoP, transitP).done(function(pubs, liftago, transit) {
+            html = html.replace('@@HOSPOD@@', pubs[0].results.length);
+            html = html.replace('@@LIFTAGO_NODE5@@', liftago[0][0].price + 'Kč');
+
+            var distancesArray = transit[0].rows[0].elements;
+            html = html.replace('@@DOJEZD_MUZEUM_MHD@@', distancesArray[0].duration.text);
+            html = html.replace('@@DOJEZD_NODE5_MHD@@', distancesArray[1].duration.text);
+
+            console.log(liftago);
+            $('body').append(html);
+          });
+        });
   });
 };
 
