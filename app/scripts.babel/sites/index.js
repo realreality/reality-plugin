7b59f8cb7509543d013fce525cba760f0dc0f396
vire <code.vire@gmail.com>
2016-11-01 00:07:37 +0100

    Added mocha tests
    * run `npm test` for just executing single time
    * run `npm test -- --watch` for development
    * no browser dep thanks to `jsdom`
    * added babel-register to support `import` in tests

--- app/scripts.babel/sites/index.js
+++ app/scripts.babel/sites/index.js
@@ -20,27 +20,25 @@ export const extractors = {
   getPrices(host) {
     if (isCurrentHost(siteHosts.SREALITY, host)) {
       const propertyParams = Array.from(document.querySelectorAll('.params li'));
+      const priceRow = propertyParams.filter(p => p.innerHTML.includes('Celková cena'))[0];
+      const areaRow = propertyParams.filter(p => p.innerHTML.includes('Užitná'))[0];
 
-      const priceRow = propertyParams.filter(p => p.innerText.includes('Celková cena'))[0];
-      const areaRow = propertyParams.filter(p => p.innerText.includes('Užitná'))[0];
-
-      const price = priceRow && parseInt(priceRow.innerText.match(/\d/g).join(''), 10);
-      const area = areaRow && parseInt(areaRow.innerText.match(/(\d){2,}/g)[0], 10);
-
+      const price = priceRow && parseInt(priceRow.innerHTML.match(/\d/g).join(''), 10);
+      const area = areaRow && parseInt(areaRow.innerHTML.match(/(\d){2,}/g)[0], 10);
       return (area && !isNaN(area) && (price && !isNaN(price))) && price / area;
     }
 
     if (isCurrentHost(siteHosts.BEZREALITKY, host)) {
       const propertyParams = Array.from(document.querySelectorAll('.box-params .row'));
-      const areaRow = propertyParams.filter(item => item.innerText.includes('plocha'))[0]; // returns DOM node
-      const priceRow = propertyParams.filter(item => item.innerText.includes('cena'))[0]; // returns DOM node
+      const areaRow = propertyParams.filter(item => item.innerHTML.includes('plocha'))[0]; // returns DOM node
+      const priceRow = propertyParams.filter(item => item.innerHTML.includes('cena'))[0]; // returns DOM node
 
       // "1.200.000 Kč" => ["1", "2", "0", "0", "0", "0", "0"] => 1200000
-      const innerText = priceRow && priceRow.querySelector('.value').innerText;
+      const innerText = priceRow && priceRow.querySelector('.value').innerHTML;
       const price = innerText && parseInt(innerText.match(/\d/g).join(''), 10);
 
       // DOMNode => "plocha:\n54 m²\n" => 54
-      const area = areaRow && parseInt(areaRow.innerText.match(/(\d){2,}/g)[0], 10);
+      const area = areaRow && parseInt(areaRow.innerHTML.match(/(\d){2,}/g)[0], 10);
       return (price && !isNaN(price)) && (area && !isNaN(area)) && price / area;
     }
 
