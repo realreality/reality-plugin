3de059bff2a940a0f210684a7bc9bcf2403452e7
vire <code.vire@gmail.com>
2016-12-03 12:40:41 +0100

    Removed local jest .vue preprocessor in favor of `jest-vue-preprocessor`

--- src/js/popup/Popup.spec.js
+++ src/js/popup/Popup.spec.js
@@ -3,30 +3,26 @@ import VueI18n from 'vue-i18n';
 import Popup from './Popup.vue';
 
 describe('Popup', () => {
+  let vm;
+
   beforeAll(() => {
     Vue.use(VueI18n);
-  });
-
-  it('should render', () => {
-    const vm = new Vue({
+    vm = new Vue({
       el: document.createElement('div'),
       render: h => h(Popup)
     });
+  });
 
+  it('should render', () => {
     expect(vm).toBeDefined();
     expect(vm.$el.querySelector('.reality-app-popup-message').textContent).toEqual('message.desc');
     expect(vm.$el.querySelector('ul').textContent).toEqual('sreality.cz bezrealitky.cz maxirealitypraha.cz');
   });
 
   it('should fire action', () => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: h => h(Popup)
-    });
-    let called = false;
-    const visit = () => { called = true; };
+    const visit = jest.fn();
     vm.$children[0].visit = visit;
     vm.$el.querySelector('a').click();
-    expect(called).toBe(true);
+    expect(visit.mock.calls[0][0]).toEqual('https://sreality.cz');
   });
 });
