d2e13ade38fa33942edb92f2cc33e15b4b7b8f4c
vire <code.vire@gmail.com>
2017-03-29 19:23:59 +0200

    chore: update dep versions

--- config/env.js
+++ config/env.js
@@ -4,7 +4,7 @@ const env = require('dotenv');
 env.load();
 
 module.exports = {
-  NODE_ENV: (process.env.NODE_ENV || 'development'),
+  
   PORT: (process.env.PORT || 3000),
   GMAPS_API_KEY: process.env.GMAPS_API_KEY,
   IPR_REST_API: (process.env.IPR_REST_API || 'https://realreality.publicstaticvoidmain.cz/rest')
