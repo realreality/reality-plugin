249e6ced0961081fb92f3d5553e4d0bb241d0b0d
Viliam Elischer <viliam.elischer@gmail.com>
2017-06-19 20:41:59 +0200

    refactor(build): Renamed files, removed dup step, improved package.json scripts (#83)

--- scripts/build-manifest.js
+++ scripts/build-manifest.js
@@ -1,13 +1,13 @@
-const fs = require('fs-extra');
-const manifest = require('../src/manifest.json');
-const packageJson = require('../package.json');
-const path = require('path');
+const fs = require('fs-extra')
+const manifest = require('../src/manifest.json')
+const packageJson = require('../package.json')
+const path = require('path')
 
 // generates the manifest file using the package.json information
 fs.outputFile(
   path.join(__dirname, '../build/manifest.json'),
   JSON.stringify(Object.assign({}, manifest, {
     description: packageJson.description,
-    version: packageJson.version
+    version: packageJson.version,
   }))
-);
+)
